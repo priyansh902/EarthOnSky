04cd32bc38093800bf4b74e2836823a506d131c6
Priyanshu Kumar <pk953666@gmail.com>
2026-05-24 19:46:04 +0530

    main integeration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:earth_on_sky/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp( MyApp());
@@ -13,6 +15,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Earth On Sky",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[700],
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        scaffoldBackgroundColor: Colors.black12,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: false,
+        ),
+      ),
+
+      home: HomeScreen(),
       
     );
   }
