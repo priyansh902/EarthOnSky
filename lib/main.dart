6c6879c63f327a49d3fd9493ce585fb8a473c62e
Priyanshu Kumar <pk953666@gmail.com>
2026-05-24 18:31:02 +0530

    materialapp in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+    
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Earth On Sky",
+      debugShowCheckedModeBanner: false,
+      
     );
   }
 }
-
